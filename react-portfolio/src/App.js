dcb2864bb7a660c2d35b01036497a85ba8588d23
Ryan Hubbuck <rhubbuck@gmail.com>
2022-04-12 12:29:58 -0400

    hero layout

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
--- react-portfolio/src/Header.js
+++ react-portfolio/src/Header.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="bg-slate-400">Header</div>
+    <div className='bg-slate-600 flex justify-between py-5 px-24'>
+        <div className='flex text-white text-lg'>
+            <h3 className='mx-2'>Ryan Hubbuck</h3>
+            <p className='mx-2'>fa</p>
+            <p className='mx-2'>ln</p>
+        </div>
+        <div>
+            <ul className='flex flex-row text-white'>
+                <li className='mx-4 text-lg cursor-pointer'>About</li>
+                <li className='mx-4 text-lg cursor-pointer'>Projects</li>
+                <li className='mx-4 text-lg cursor-pointer'>Contact</li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
