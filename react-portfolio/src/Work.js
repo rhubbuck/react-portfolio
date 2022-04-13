a78684a66c87623181c68b5a00a74cb1bb3797d7
Ryan Hubbuck <rhubbuck@gmail.com>
2022-04-13 19:17:48 -0400

    work section hover effect

--- react-portfolio/src/Work.js
+++ react-portfolio/src/Work.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 import Nama from './images/logo192.png';
 
 const Work = () => {
+
+    const [style, setStyle] = useState({display: 'none'});
+
+    
   return (
     <div>
         <header>My Latest Work</header>
         <div className='grid grid-cols-2 mx-44 gap-x-5'>
-            <div className='h-9 bg-red-500 work-image'></div>
-            <div className='h-9 bg-red-500 work-image'></div>
-            <div className='h-9 work-image cursor-pointer'></div>
-            <div className='h-9 work-image cursor-pointer'></div>
-            <div className='h-9 work-image cursor-pointer'></div>
-            <div className='h-9 work-image cursor-pointer'></div>        
+            <div className='h-9 bg-red-500 work-image cursor-pointer'>
+                <div className='h-full w-full bg-white opacity-0 hover:opacity-100 transition-all duration-150'>
+                    <h3 className='pt-10'>Nama Sushi</h3>
+                    <button className='border-2 mt-10'>Learn More</button>
+                </div>
+            </div>
+            <div className='h-9 bg-red-500 work-image cursor-pointer'>
+                <div className='h-full w-full bg-white opacity-0 hover:opacity-100 transition-all duration-150'>
+                    <h3 className='pt-10'>Nama Sushi</h3>
+                    <button className='border-2 mt-10'>Learn More</button>
+                </div>
+            </div>
+            <div className='h-9 work-image cursor-pointer'>
+                <div className='h-full w-full bg-white opacity-0 hover:opacity-100 transition-all duration-150'>
+                    <h3 className='pt-10'>Nama Sushi</h3>
+                    <button className='border-2 mt-10'>Learn More</button>
+                </div>
+            </div>
+            <div className='h-9 work-image cursor-pointer'>
+                <div className='h-full w-full bg-white opacity-0 hover:opacity-100 transition-all duration-150'>
+                    <h3 className='pt-10'>Nama Sushi</h3>
+                    <button className='border-2 mt-10'>Learn More</button>
+                </div>
+            </div>
+            <div className='h-9 work-image cursor-pointer'>
+                <div className='h-full w-full bg-white opacity-0 hover:opacity-100 transition-all duration-150'>
+                    <h3 className='pt-10'>Nama Sushi</h3>
+                    <button className='border-2 mt-10'>Learn More</button>
+                </div>
+            </div>
+            <div className='h-9 work-image cursor-pointer'>
+                <div className='h-full w-full bg-white opacity-0 hover:opacity-100 transition-all duration-150'>
+                    <h3 className='pt-10'>Nama Sushi</h3>
+                    <button className='border-2 mt-10'>Learn More</button>
+                </div>
+            </div>        
         </div>
     </div>
   )
