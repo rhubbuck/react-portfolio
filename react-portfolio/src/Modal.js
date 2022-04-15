1c231a20deea146f0512a277479b05cbfb6626e4
Ryan Hubbuck <rhubbuck@gmail.com>
2022-04-15 18:35:45 -0400

    passing props to modal and open/close

--- react-portfolio/src/Modal.js
+++ react-portfolio/src/Modal.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import './index.css';
 
-const Modal = ({handleClick, title, subtitle, description, link, image}) => {
-    console.log(image)
+const Modal = ({handleClick, title, subtitle, description, link, image1, image2, image3}) => {
+    let photo1 = require(`${image1}`);
+    let photo2 = require(`${image2}`);
+    let photo3 = require(`${image3}`);
+
   return (
     <div className='fixed z-30 top-0 left-0 bottom-0 w-full background-shade'>
-        <div className='bg-white p-5 h-4/5 mt-20 w-2/5 mx-auto rounded-md flex flex-row'>
+        <div className='bg-white p-5 h-4/5 mt-20 w-2/5 mx-auto rounded-md flex flex-col'>
             <div className='flex justify-between flex-col'>
-                <div className='w-64 h-40 bg-slate-500' style={{backgroundImage: `url(require("images/nama1.png"))`}}></div>
-                <div className='w-64 h-40 bg-slate-600'>image</div>
-                <div className='w-64 h-40 bg-slate-700'>image</div>
+                <div className=' w-2/3 h-52 mx-auto rounded' style={{
+                    backgroundImage: "url("+photo1+")",
+                    backgroundSize: "cover"
+                    }}></div>
+                {/* <div className='w-64 h-40' style={{
+                    backgroundImage: "url("+photo2+")",
+                    backgroundSize: "contain"
+                    }}></div>
+                <div className='w-64 h-40' style={{
+                    backgroundImage: "url("+photo3+")",
+                    backgroundSize: "contain"
+                    }}></div> */}
             </div>
             <div className='flex flex-col justify-between items-end'>
                 <div>
                     <p className='mt-1 text-4xl font-semibold'>{title}</p>
                     <p className='text-xl mt-5'>{subtitle}</p>
                     <p className='text-lg mt-5 mx-2'>{description}</p>
-                    <p className='text-md underline mt-5'>{link}</p>
+                    <p className='text-md underline mt-5 cursor-pointer'>{link}</p>
                     <div>
                         <button type="button" className="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out mx-2 mt-4">Live Site</button>
                         <button type="button" className="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out mx-2 mt-4">Source Code</button>
--- react-portfolio/src/Work.js
+++ react-portfolio/src/Work.js
@@ -10,7 +10,9 @@ const Work = () => {
     const [ subtitle, setSubtitle] = useState('');
     const [ description, setDescription] = useState('');
     const [ link, setLink] = useState('');
-    const [ image, setImage] = useState();
+    const [ image1, setImage1] = useState();
+    const [ image2, setImage2] = useState();
+    const [ image3, setImage3] = useState();
 
     const handleClick = () => {
         setModalOpen(false);
@@ -21,7 +23,9 @@ const Work = () => {
         setSubtitle('React / Firebase');
         setDescription('Mock restaurant landing page with restaurant information and menus. Reservation information is saved into a backend database to be reviewed and handled by restaurant management/staff.');
         setLink('https://github.com/rhubbuck/react-restaurant');
-        setImage({Nama})
+        setImage1('./images/nama1.png');
+        setImage2('./images/nama2.png');
+        setImage3('./images/nama3.png');
         setModalOpen(true);
     }
 
@@ -66,7 +70,15 @@ const Work = () => {
                 </div>
             </div>        
         </div>
-        {modalOpen && <Modal handleClick={handleClick} title={title} subtitle={subtitle} description={description} link={link} image={image} />}
+        {modalOpen && <Modal 
+                            handleClick={handleClick} 
+                            title={title} subtitle={subtitle} 
+                            description={description} 
+                            link={link} 
+                            image1={image1} 
+                            image2={image2} 
+                            image3={image3}
+                            />}
     </div>
   )
 }
